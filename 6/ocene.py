1c0caf5d41b843e1d6b7503f74de2d7ba93fa4a7
Gregor Majcen <majcn.m@gmail.com>
2012-04-25 17:51:04 +0200

    popravljen logLoss. dodani learnerji

--- 6/ocene.py
+++ 6/ocene.py
@@ -11,14 +11,20 @@ def cvkf(learner, data, k):
 	return res
 
 def logLoss(T,P):
+	T = np.array(T)
+	P = np.array(P)
+	P = P*0.99999999998+0.00000000001
 	return -1./T.size*sum(T*np.log(P)+(1-T)*np.log(1-P))
 
-def crossValidation(learners, data, k=5):
+def crossValidation(learners, data, k=10):
 	_, t, _ = data.to_numpy()
 	for i in learners:
 		p = cvkf(i, data, k)
 		print "%10s: %5.3f" % (i.name, logLoss(t,p))
 
 data = Orange.data.Table("data/train.tab")
-lr = Orange.classification.logreg.LogRegLearner(remove_singular=1)
-crossValidation([lr], data)
+lr = Orange.classification.logreg.LogRegLearner(remove_singular=1, name="logreg")
+rf = Orange.ensemble.forest.RandomForestLearner(trees=50, name="forest")
+knn = Orange.classification.knn.kNNLearner(name="knn")
+lsvm = Orange.classification.svm.LinearSVMLearner(name="linearSVM")
+crossValidation([lr, rf, knn], data)
